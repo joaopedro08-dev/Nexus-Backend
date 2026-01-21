481211870a49102f2f7f6b15116d418d520154fd
João Pedro <joaopedrodaladeamello@gmail.com>
2026-01-21 03:03:47 -0300

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ['http://localhost:3000', 'https://anotherdomain.com'],
+  origin: ['http://localhost:3000', 'https://nexus-ai-rouge.vercel.app'],
   methods: ['POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: false,
